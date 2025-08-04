f7bedf0f1f32350c84f405a1b32bcb5fb153bc05
angheloamir <angheloamir@gmail.com>
2025-08-04 21:54:59 +0800

    remove unused code

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ This project is started from using a boilerplate that I made called
 "Express.js Vercel Starter"
 
 ## Purpose
-This application is a showcase of my skills
+This application is a showcase of my skills.
+
+## Usage
+type `npm install`
+then `npm run dev`
+to run the server
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,16 @@
 import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import path from 'path';
+import cors    from 'cors';
+import morgan  from 'morgan';
+import path    from 'path';
 
-import Test from './routes/test';
+//List of routes
+import Test    from './routes/test';
 
+//Initial set up
 const PORT   = process.env.PORT || 8000;
 const server = express();
 server.use(morgan('dev'));
 server.use(cors());
-
-//import helmet from 'helmet';
-// server.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         defaultSrc: ["'self'"],
-//         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-//         styleSrc: ["'self'", 'fonts.googleapis.com'],
-//       },
-//     },
-//   })
-// );
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
@@ -29,7 +18,6 @@ server.use(express.static(path.join(__dirname, '../public')));
 
 //Set the route paths
 server.use('/api/v1/test', Test);
-
 
 server.listen(PORT, () =>
   console.log(`Server running on: http://localhost:${PORT}`)
